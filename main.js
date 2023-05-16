55abc34b39d5f475e4da27a1b0a015df21fd7da4
Vadim-Cr <vadim.crudu@gmail.com>
2023-05-16 18:57:05 +0200

    benchmarck 1 e 2

---
+++ main.js
@@ -0,0 +1,119 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+
+// Milestone 1 
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// id del post, numero progressivo da 1 a n
+// aganciamoci al container "post" vuoto
+
+posts.forEach(processPost);
+
+function processPost(post) {
+  
+const userPost = document.querySelector(".posts-list");
+console.log(userPost);
+
+// const post = posts[0]; // Seleziona il primo oggetto dall'array posts
+
+// todo Aggiungere tutto dentro ad un literal
+// nome autore
+// foto autore,
+// data in formato americano (mm-gg-yyyy),
+// testo del post,
+// immagine (non tutti i post devono avere una immagine),
+// numero di likes.
+const myUserPost = document.createElement("div");
+myUserPost.classList.add("post")
+
+myUserPost.innerHTML = `
+                        <div class="post__header">
+                            <div class="post-meta">
+                                <div class="post-meta__icon">
+                                    <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}'s image">                    
+                                </div>
+                                <div class="post-meta__data">
+                                    <div class="post-meta__author">${post.author.name}</div>
+                                    <div class="post-meta__time">${post.created}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="post__text">${post.content}</div>
+                        <div class="post__image">
+                             <img src=${post.media} alt="${post.author.name}'s image">
+                        </div>
+                        <div class="post__footer">
+                            <div class="likes js-likes">
+                                <div class="likes__cta">
+                                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                        <span class="like-button__label">Mi Piace</span>
+                                    </a>
+                                </div>
+                                <div class="likes__counter">
+                                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                                </div>
+                            </div> 
+                        </div>
+                        `;
+userPost.append(myUserPost);
+};
+
+const clickLike = document.querySelector(".likes__cta");
+clickLike.addEventListener("click", function() {
+posts.likes++});
